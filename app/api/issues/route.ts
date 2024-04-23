0d28a8e57a46a10c0860b0d465a4a3501764cd42
Praneeth Gamage <praneethgamage@gmail.com>
2024-04-23 12:05:16 +0530

    Build an api for creating issues

---
+++ app/api/issues/route.ts
@@ -0,0 +1,30 @@
+/** @format */
+
+import { NextRequest, NextResponse } from 'next/server';
+import { z } from 'zod';
+import prisma from '@/prisma/client';
+
+//Issue Schema object to validate request body
+const createIssueSchema = z.object({
+  title: z.string().min(1).max(255),
+  description: z.string().min(1),
+});
+
+export async function POST(requeset: NextRequest) {
+  // Get the request body
+  const body = await requeset.json();
+
+  // Validte request body
+  const validation = createIssueSchema.safeParse(body);
+  if (!validation.success)
+    return NextResponse.json(validation.error.errors, { status: 400 });
+
+  const newIssue = await prisma.issue.create({
+    data: {
+      title: body.title,
+      description: body.description,
+    },
+  });
+
+  return NextResponse.json(newIssue, { status: 201 });
+}
